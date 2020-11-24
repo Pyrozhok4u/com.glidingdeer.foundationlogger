c915031773e3cd2757512528c92cc672281f6bbc
Vadym <vadym.p@glidingdeer.com>
2020-11-24 14:17:50 +0200

    Operation utils nested file added

---
+++ OperationUtils/SomeNestedUtils/SomeNestedClass.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Foundation.Logger;
+
+namespace Foundation.Utils.OperationUtils.SomeNestedUtils
+{
+    public class SomeNestedClass
+    {
+        private int _test = 12;
+        public int Test => _test;
+        public static string TestString = "StaticTestString";
+        public string TestName = "Public test name";
+    }
+}
